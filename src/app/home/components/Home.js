6c359cd2e068c1506645b4a50faae8260369c76e
Rodriguez <emanuel.rodriguez@externos-ar.cencosud.com>
2020-10-16 11:50:06 -0300

    reestructuracion y routes

---
+++ src/app/home/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+// Styles
+import styles from './Home.scss';
+
+class Home extends Component {
+    render () {
+        return (
+            <div className={styles.Home}>
+                Home - <Link to="/about">About</Link>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ src/shared/routes.js
@@ -0,0 +1,20 @@
+// Components
+import Home from '../app/home/components/Home';
+import About from '../app/about/components/About';
+
+// Containers
+// import Blog from '../app/blog';
+
+const routes = [
+    {
+		path: '/',
+		component: Home,
+		exact: true
+	},
+	{
+		path: '/about',
+		component: About
+	}
+];
+
+export default routes;
